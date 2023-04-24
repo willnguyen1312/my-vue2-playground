33e0e84506d1afc68b724e29288cd71e35107b1d
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-04-24 20:09:39 -0300

    Initial commit

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,18 @@
+import { render, screen, fireEvent } from "@testing-library/vue";
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    render(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    // expect(wrapper.text()).toMatch(msg);
+    expect(
+      screen.getByRole("heading", {
+        name: /new message/i,
+      })
+    ).toBeInTheDocument();
   });
 });
